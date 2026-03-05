1ace1ad09d74786d576b30a549cc5711a3e6f591
rigelia <f3atha04@gmail.com>
2026-03-05 21:20:29 +0700

    update

--- scenes/AreaTrigger.gd
+++ scenes/AreaTrigger.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@export var sceneName: String = "Level 1"
+@export var sceneName: String = "WinScreen"
 
 func _on_Area_Trigger_body_entered(body):
     if body.get_name() == "Player":
